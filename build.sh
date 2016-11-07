813074d3fb409c402307fe765030916dd0575e3a
Ahmed Samy <f.fallen45@gmail.com>
2016-11-07 06:23:20 +0000

    add build script and license
    Signed-off-by: Ahmed Samy <f.fallen45@gmail.com>

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CORES=`grep processor /proc/cpuinfo | wc -l`
+MAKEOPT=$(($CORES + 1))
+MAKE=make
+
+$MAKE C=1 -j$MAKEOPT
+if [[ "$?" -eq "0" ]]
+then
+	echo "Compilation successful.";
+else
+	echo "Compilation failed: $?";
+fi
+
