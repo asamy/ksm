3ba347ba8c8773e9566b1f63e630b64be72cb312
Ahmed Samy <f.fallen45@gmail.com>
2016-11-15 14:45:29 +0200

    add .gitignore and .gitattributes
    Signed-off-by: Ahmed Samy <f.fallen45@gmail.com>

--- compiler.h
+++ compiler.h
@@ -45,12 +45,12 @@
 /* Long names  */
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
+typedef unsigned long int uint32_t;
 typedef unsigned long long uint64_t;
 
 typedef signed char sint8_t;
 typedef signed short sint16_t;
-typedef signed int sint32_t;
+typedef signed long int sint32_t;
 typedef signed long long sint64_t;
 
 /* Short names  */
