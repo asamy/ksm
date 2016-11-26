ec1ccb0f6f600ee53712d6122d89c65f98ca323c
Ahmed Samy <f.fallen45@gmail.com>
2016-11-26 17:04:02 +0000

    dpc: STATIC_CALL_DPC can only take 1 argument
    Signed-off-by: Ahmed Samy <f.fallen45@gmail.com>

--- dpc.h
+++ dpc.h
@@ -19,11 +19,6 @@
 #ifndef __KERNEL_DPC_H
 #define __KERNEL_DPC_H
 
-#ifndef _MSC_VER
-#define _In_
-#define _In_opt_
-#endif
-
 NTKERNELAPI
 #ifndef __GNUC__
 _IRQL_requires_max_(APC_LEVEL)
@@ -66,10 +61,11 @@ static NTSTATUS __g_dpc_logical_rval = 0;
 		KeSignalCallDpcDone(sys0);	\
 	}	\
 
-#define STATIC_CALL_DPC(name, ...) do {	\
+#define STATIC_CALL_DPC(name, ctx) do {	\
 	__g_dpc_logical_rval = 0;	\
-	KeGenericCallDpc(__percpu_##name, __VA_ARGS__);	\
+	KeGenericCallDpc(__percpu_##name, (ctx));	\
 } while (0)
 #define STATIC_DPC_RET() 	__g_dpc_logical_rval
 
 #endif
+
