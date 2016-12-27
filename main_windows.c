c57de02abc4ffc7fc9d862d05e29805e4da0d1b3
Ahmed Samy <asamy@protonmail.com>
2016-12-27 21:35:12 +0200

    Take pe.h out of the equation.
    No real needed right now (at least).
    
    Signed-off-by: Ahmed Samy <asamy@protonmail.com>

--- main_windows.c
+++ main_windows.c
@@ -20,7 +20,22 @@
 #include <intrin.h>
 
 #include "ksm.h"
-#include "pe.h"
+
+typedef struct _LDR_DATA_TABLE_ENTRY {
+	LIST_ENTRY InLoadOrderLinks;
+	LIST_ENTRY InMemoryOrderLinks;
+	LIST_ENTRY InInitializationOrderLinks;
+	PVOID DllBase;
+	PVOID EntryPoint;
+	ULONG SizeOfImage;
+	UNICODE_STRING FullDllName;
+	UNICODE_STRING BaseDllName;
+	ULONG Flags;
+	USHORT LoadCount;
+	USHORT TlsIndex;
+	LIST_ENTRY HashLinks;
+	ULONG TimeDateStamp;
+} LDR_DATA_TABLE_ENTRY, *PLDR_DATA_TABLE_ENTRY;
 
 #ifdef ENABLE_RESUBV
 static DEV_EXT g_dev_ext = { NULL, NULL };
