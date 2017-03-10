bc77625e0638d81f6dc93209c9798bb68f1602d0
Ahmed Samy <asamy@protonmail.com>
2017-03-10 18:10:20 +0200

    mingw: some feedback when changing to nonpagable

--- make_nonpagable.py
+++ make_nonpagable.py
@@ -6,9 +6,10 @@ import pefile
 pe = pefile.PE(sys.argv[1])
 for section in pe.sections:
     if (section.Characteristics & 0x20) != 0:
-        print("Changing section %s to be non-pageable (attr: 0x%08X)\n" % (section.Name,
+        print("%s: changing section %s to be non-pageable (attr: 0x%08X)" % (sys.argv[0], section.Name,
             section.Characteristics))
         section.Characteristics |= 0x08000000
 
 pe.write(sys.argv[1])
+print("%s: wrote %s" % (sys.argv[0], sys.argv[1]))
 
