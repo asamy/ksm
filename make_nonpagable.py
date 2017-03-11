b3ec0429e40e9e4fe1e2ae270c2d97f81ab5354e
Ahmed Samy <asamy@protonmail.com>
2017-03-11 11:14:23 +0200

    mingw/make_nonpagable: quitten it

--- make_nonpagable.py
+++ make_nonpagable.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
-
+# Mark executable sections as non-pagable, this is necessary because VMX root mode runs
+# with interrupts off, so all pages must be physically contiguous.
+# This file is only for the MinGW build!
 import sys
 import pefile
 
 pe = pefile.PE(sys.argv[1])
 for section in pe.sections:
     if (section.Characteristics & 0x20) != 0:
-        print("%s: changing section %s to be non-pageable (attr: 0x%08X)" % (sys.argv[0], section.Name,
-            section.Characteristics))
         section.Characteristics |= 0x08000000
 
 pe.write(sys.argv[1])
-print("%s: wrote %s" % (sys.argv[0], sys.argv[1]))
 
